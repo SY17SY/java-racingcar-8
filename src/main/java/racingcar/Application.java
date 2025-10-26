342d0e0c0279ac768b4d5301a3dd9cd9ec351ce0
yeong <rlatjdud6573@naver.com>
2025-10-27 00:04:25 +0900

    feat: Application 의존성 주입

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.RacingcarController;
+import racingcar.domain.Racingcar;
+import racingcar.view.ConsoleInput;
+import racingcar.view.ConsoleOutput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleInput consoleInput = new ConsoleInput();
+        ConsoleOutput consoleOutput = new ConsoleOutput();
+        Racingcar racingcar = new Racingcar();
+
+        RacingcarController controller = new RacingcarController(consoleInput, consoleOutput, racingcar);
+
+        controller.runController();
     }
 }
