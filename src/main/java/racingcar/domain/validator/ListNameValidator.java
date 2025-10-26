62ed00e6a404098ffad8c07341f625ab04fb6413
yeong <rlatjdud6573@naver.com>
2025-10-27 01:11:33 +0900

    feat: ListName IAE - isOverFive 구현

--- src/main/java/racingcar/domain/validator/ListNameValidator.java
+++ src/main/java/racingcar/domain/validator/ListNameValidator.java
@@ -7,12 +7,21 @@ import java.util.List;
 public class ListNameValidator {
     public static void validate(List<String> names) {
         isUnique(names);
+        for (String name : names) {
+            isOverFive(name);
+        }
     }
 
     private static void isUnique(List<String> names) {
         long distinctCount = names.stream().distinct().count();
         if (distinctCount != names.size()) {
             throw new IllegalArgumentException(ERROR_MSG_NAME_SAME);
+        }
+    }
+
+    private static void isOverFive(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(ERROR_MSG_NAME_LENGTH);
         }
     }
 }
