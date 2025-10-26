5b4fa2ffdcd54564fb419e07957ceba7d599987e
yeong <rlatjdud6573@naver.com>
2025-10-27 01:14:15 +0900

    refactor: final 추가, VALID_PATTERN 정규식 추가

--- src/main/java/racingcar/util/Constants.java
+++ src/main/java/racingcar/util/Constants.java
@@ -1,10 +1,11 @@
 package racingcar.util;
 
 public class Constants {
-    public static int MOVE_FORWARD_NUM = 4;
-    public static int RANDOM_NUM_START = 0;
-    public static int RANDOM_NUM_END = 9;
+    public static final int MOVE_FORWARD_NUM = 4;
+    public static final int RANDOM_NUM_START = 0;
+    public static final int RANDOM_NUM_END = 9;
 
-    public static String DELIMITER = ",";
-    public static String PROGRESS = "-";
+    public static final String DELIMITER = ",";
+    public static final String PROGRESS = "-";
+    public static final String VALID_PATTERN = "^[A-Za-z0-9]+$";
 }
