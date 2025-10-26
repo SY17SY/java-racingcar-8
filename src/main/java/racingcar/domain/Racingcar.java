6e294fdb5b14b96424aefb4a28637efd55bb8bea
yeong <rlatjdud6573@naver.com>
2025-10-27 00:03:53 +0900

    feat: Racingcar 경주 준비 기능 구현

---
+++ src/main/java/racingcar/domain/Racingcar.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import static racingcar.util.Constants.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import racingcar.model.*;
+
+public class Racingcar {
+    public List<String> parseNames(String inputName) {
+        return Arrays.stream(inputName.split(DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
+
+    public Cars createCars(List<String> names) {
+        return new Cars(names.stream().map(Car::new).toList());
+    }
+}
