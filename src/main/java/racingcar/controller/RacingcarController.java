e80dfe168d44967779a439030af72bd54a745c6b
yeong <rlatjdud6573@naver.com>
2025-10-27 00:04:15 +0900

    feat: controller 경주 준비 기능 구현

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,20 +1,28 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.Racingcar;
+import racingcar.model.Cars;
 import racingcar.view.ConsoleInput;
 import racingcar.view.ConsoleOutput;
 
 public class RacingcarController {
     private final ConsoleInput consoleInput;
     private final ConsoleOutput consoleOutput;
+    private final Racingcar racingcar;
 
-    RacingcarController(ConsoleInput consoleInput, ConsoleOutput consoleOutput) {
+    public RacingcarController(ConsoleInput consoleInput, ConsoleOutput consoleOutput, Racingcar racingcar) {
         this.consoleInput = consoleInput;
         this.consoleOutput = consoleOutput;
+        this.racingcar = racingcar;
     }
 
     public void runController() {
         consoleOutput.printPromptName();
         String inputName = consoleInput.inputName();
+
+        List<String> names = racingcar.parseNames(inputName);
+        Cars cars = racingcar.createCars(names);
 
         consoleOutput.printPromptCount();
         int count = consoleInput.inputCount();
