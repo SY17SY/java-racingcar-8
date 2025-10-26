e1967de583bb5120d4bbb6e421e79a9a7d3291a7
yeong <rlatjdud6573@naver.com>
2025-10-26 23:54:01 +0900

    fix: Car 생성자 -> public

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -8,7 +8,7 @@ public class Car {
     private final String name;
     private int position;
 
-    Car(String name) {
+    public Car(String name) {
         this.name = name;
         this.position = 0;
     }
