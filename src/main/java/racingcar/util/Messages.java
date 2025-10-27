39d345c78abc5f2eb3df290f73dd0c75bc2c3785
yeong <rlatjdud6573@naver.com>
2025-10-27 10:29:56 +0900

    docs: Messages에 주석 추가

--- src/main/java/racingcar/util/Messages.java
+++ src/main/java/racingcar/util/Messages.java
@@ -6,12 +6,16 @@ public class Messages {
     public static final String PROMPT_MSG_EXECUTION = "실행 결과";
     public static final String PROMPT_MSG_WINNER_ANNOUNCEMENT = "최종 우승자 : ";
 
+    // Input Name Validator
     public static final String ERROR_MSG_NAME_MISSING_COMMA = "자동차 이름이 쉼표(,)로 구분되지 않았습니다.";
     public static final String ERROR_MSG_NAME_EMPTY = "자동차 이름은 빈 문자열이 될 수 없습니다.";
+
+    // List Name Validator
     public static final String ERROR_MSG_NAME_SAME = "자동차 이름은 모두 달라야 합니다.";
     public static final String ERROR_MSG_NAME_LENGTH = "자동차 이름은 5자 이하만 가능합니다.";
     public static final String ERROR_MSG_NAME_INVALID = "자동차 이름은 영어 대/소문자나 숫자로만 이루어질 수 있습니다.";
 
+    // Input Count Validator
     public static final String ERROR_MSG_COUNT_EMPTY = "시도할 횟수를 작성해 주십시오.";
     public static final String ERROR_MSG_COUNT_NEGATIVE = "시도할 횟수는 음수가 될 수 없습니다.";
     public static final String ERROR_MSG_COUNT_INVALID = "시도할 횟수에 문자가 포함될 수 없습니다.";
